78f4854b337de1125154d099c3189c1ab418e7e2
liangmayong <liangmayong@surevideo.me>
2018-03-19 21:20:58 +0800

    fix

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 BUILD_DIR=$(pwd)/build
+DATA_DIR=$(pwd)/data
 EMQ_DIR=$BUILD_DIR/emq-relx
 EMQ_REPO_GIT=https://github.com/emqtt/emq-relx
 EMQ_REPO_GIT_VERSION=2.3.5
@@ -12,7 +13,7 @@ echo ""
 mkdir $BUILD_DIR
 fi
 
-echo "Pull emq-relx from git"
+echo " ================> Pull emq-relx from git"
 
 if [ -d $EMQ_DIR ]; then
 cd $EMQ_DIR
@@ -22,10 +23,15 @@ cd $BUILD_DIR
 git clone -b release $EMQ_REPO_GIT
 fi
 
-echo "Replacement Makefile configuration"
+echo " ================> Replacement Makefile configuration"
 
+cd $(pwd)
+cp $DATA_DIR/Makefile $EMQ_DIR/Makefile
+
+
+echo " ================> Make"
 cd $EMQ_DIR
 
-make
+# make
 
 
