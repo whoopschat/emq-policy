c78f5bc7b82417eada3f0a5546e12dc7aa704491
liangmayong <liangmayong@surevideo.me>
2018-03-20 11:11:12 +0800

    add pull.sh

---
+++ pull.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo -e
+echo "======================================="
+echo "|        Pull project from git        |"
+echo "======================================="
+echo -e
+
+git fetch --all
+git reset --hard origin/master
+git pull
+
+chmod 777 -R release.sh
