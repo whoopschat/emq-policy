3d5b52e7ffaafc37f16c9fa3e27274b2772d97f8
liangmayong <liangmayong@surevideo.me>
2018-03-20 10:39:44 +0800

    fix

--- build.sh
+++ build.sh
@@ -4,9 +4,13 @@ CURRENT_DIR=$(pwd)
 BUILD_DIR=${CURRENT_DIR}/build
 DATA_DIR=${CURRENT_DIR}/data
 EMQ_DIR=${BUILD_DIR}/emq-relx
-EMQ_REPO_GIT=https://github.com/emqtt/emq-relx
-EMQ_REPO_GIT_VERSION=2.3.5
+
+EMQ_REL_X_REPO_GIT=https://github.com/emqtt/emq-relx
+EMQ_REL_X_REPO_GIT_BRANCH=release
+EMQ_REL_X_REPO_GIT_VERSION=2.3.5
+
 EMQ_BUILD_DIR=${EMQ_DIR}/_rel/emqttd
+EMQ_BUILD_TARGET=${BUILD_DIR}/release_emqttd_${EMQ_REL_X_REPO_GIT_VERSION}.zip
 
 if [ ! -d ${BUILD_DIR} ]; then
 mkdir ${BUILD_DIR}
@@ -16,21 +20,28 @@ if [ -d ${EMQ_BUILD_DIR} ]; then
 rm -rf ${EMQ_BUILD_DIR}
 fi
 
+if [ -d ${EMQ_BUILD_TARGET} ]; then
+rm -rf ${EMQ_BUILD_TARGET}
+fi
+
 echo "======================================="
 echo "|       Pull emq-relx from git        |"
-echo "=======================================\n"
+echo "======================================="
+echo -e
 
 if [ -d ${EMQ_DIR} ]; then
 cd ${EMQ_DIR}
 git pull
 else
 cd ${BUILD_DIR}
-git clone -b release ${EMQ_REPO_GIT}
+git clone -b ${EMQ_REL_X_REPO_GIT_BRANCH} ${EMQ_REL_X_REPO_GIT}
 fi
 
 echo "======================================="
 echo "|               Building              |"
-echo "=======================================\n"
+echo "======================================="
+echo -e
+
 cd ${CURRENT_DIR}
 cp ${DATA_DIR}/Makefile ${EMQ_DIR}/Makefile
 cp ${DATA_DIR}/relx.config ${EMQ_DIR}/relx.config
@@ -40,6 +51,11 @@ make
 
 echo "======================================="
 echo "|            Compression              |"
-echo "=======================================\n"
-zip -r -q -o ${BUILD_DIR}/emqttd.zip  ${EMQ_BUILD_DIR}
+echo "======================================="
+echo -e
+
+cd ${EMQ_BUILD_DIR}
+zip -r -q -o ${EMQ_BUILD_TARGET}  ./
+
+echo "Done, release path : ${EMQ_BUILD_TARGET}"
 
