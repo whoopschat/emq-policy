bf4a2ec63c4a64e2c5f3fa2b0f67ed1e4d075f64
liangmayong <liangmayong@surevideo.me>
2018-03-22 18:55:06 +0800

    fix

--- src/emq_policy_server_module_auth.erl
+++ src/emq_policy_server_module_auth.erl
@@ -131,7 +131,7 @@ handleAuthPub(ClientPid, ClientId, Username, _PubList) ->
   ok.
 
 publishMessage(_ClientPid, ClientId, Username, Topic, Payload) ->
-  Msg = emqttd_message:make(list_to_binary(Username ++ "/" ++ binary_to_list(ClientId)), 1, Topic, Payload),
+  Msg = emqttd_message:make(list_to_binary(binary_to_list(Username) ++ "/" ++ binary_to_list(ClientId)), 1, Topic, Payload),
   emqttd:publish(Msg#mqtt_message{retain = false}).
 
 description() -> "Emq Policy Server AUTH module".
