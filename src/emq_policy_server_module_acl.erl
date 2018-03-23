b9d18297ec01e6539310fdbfc30f72bb7fca75ca
liangmayong <liangmayong@surevideo.me>
2018-03-23 14:29:50 +0800

    fix bugs

--- src/emq_policy_server_module_acl.erl
+++ src/emq_policy_server_module_acl.erl
@@ -45,17 +45,17 @@ check_acl({#mqtt_client{client_id = ClientId}, PubSub, Topic}, _Env) ->
 reload_acl(_State) -> ok.
 
 access(subscribe, ClientId, Topic) ->
-  log("acl log (subscribe.acl):~nclientId:~s topic: ~s deny~n=====================================================~n", [ClientId, Topic]),
+  log("~nacl log (subscribe.acl):~nclientId:~s topic: ~s deny~n=====================================================~n", [ClientId, Topic]),
   deny;
 access(publish, ClientId, Topic) ->
   App = parser_app_by_clientId(ClientId),
   IsTopic = string:str(binary_to_list(Topic), "$" ++ App ++ "/") > 0,
   if
     IsTopic ->
-      log("acl log (publish.acl):~nclientId:~s topic: ~s allow~n=====================================================~n", [ClientId, Topic]),
+      log("~nacl log (publish.acl):~nclientId:~s topic: ~s allow~n=====================================================~n", [ClientId, Topic]),
       allow;
     true ->
-      log("acl log (publish.acl):~nclientId:~s topic: ~s deny~n=====================================================~n", [ClientId, Topic]),
+      log("~nacl log (publish.acl):~nclientId:~s topic: ~s deny~n=====================================================~n", [ClientId, Topic]),
       deny
   end.
 
--- src/emq_policy_server_module_hook.erl
+++ src/emq_policy_server_module_hook.erl
@@ -28,8 +28,8 @@
 -include("emq_policy_server.hrl").
 -include_lib("emqttd/include/emqttd.hrl").
 
--import(emq_policy_server_util_format, [parser_app_by_clientId/1, parser_device_by_clientId/1, parser_username_by_clientId/1, validate_clientId/2, format_from/1]).
--import(emq_policy_server_util_http, [request/3, env_http_request/0]).
+-import(emq_policy_server_util_format, [parser_app_by_clientId/1, parser_device_by_clientId/1, parser_username_by_clientId/1, validate_clientId/2, replace_str/3, format_from/1]).
+-import(emq_policy_server_util_http, [request/3, requestSync/3, env_http_request/0]).
 -import(emq_policy_server_util_logger, [log/2]).
 
 -export([load/1, unload/0]).
@@ -58,13 +58,13 @@ unload() ->
 
 %% hook client connected
 hook_client_connected(ConnAck, Client = #mqtt_client{client_id = ClientId}, _Env) ->
-  log("connect log (client.connected):~nclient ~s connected, connack: ~w~n=====================================================~n", [ClientId, ConnAck]),
+  log("~nconnect log (client.connected):~nclient ~s connected, connack: ~w~n=====================================================~n", [ClientId, ConnAck]),
   request_connect_hook(Client, client_connected, env_http_request()),
   {ok, Client}.
 
 %% hook client connected
 hook_client_disconnected(Reason, Client = #mqtt_client{client_id = ClientId}, _Env) ->
-  log("connect log (client.disconnected):~nclient ~s disconnected, reason: ~w~n=====================================================~n", [ClientId, Reason]),
+  log("~nconnect log (client.disconnected):~nclient ~s disconnected, reason: ~w~n=====================================================~n", [ClientId, Reason]),
   request_connect_hook(Client, client_disconnected, env_http_request()),
   ok.
 
@@ -77,20 +77,20 @@ hook_message_publish(Message = #mqtt_message{topic = <<"$SYS/", _/binary>>}, _En
   {ok, Message};
 
 hook_message_publish(Message = #mqtt_message{topic = Topic, payload = Payload}, _Env) ->
-  log("message log (message.publish):~npublish ~s~n=====================================================~n", [emqttd_message:format(Message)]),
+  log("~nmessage log (message.publish):~npublish ~s~n=====================================================~n", [emqttd_message:format(Message)]),
   {FromClientId, FromUsername} = format_from(Message#mqtt_message.from),
   request_message_hook(Topic, Payload, FromClientId, FromUsername, message_publish, env_http_request()),
   {ok, Message}.
 
 %% hook message delivered
 hook_message_delivered(ClientId, Username, Message = #mqtt_message{topic = Topic, payload = Payload}, _Env) ->
-  log("message log (message.delivered):~ndelivered to client(~s/~s): ~s~n=====================================================~n", [Username, ClientId, emqttd_message:format(Message)]),
+  log("~nmessage log (message.delivered):~ndelivered to client(~s/~s): ~s~n=====================================================~n", [Username, ClientId, emqttd_message:format(Message)]),
   request_message_hook(Topic, Payload, ClientId, Username, message_delivered, env_http_request()),
   {ok, Message}.
 
 %% hook message ask
 hook_message_ack(ClientId, Username, Message = #mqtt_message{topic = Topic, payload = Payload}, _Env) ->
-  log("message log (message.acked):~nclient(~s/~s) acked: ~s~n=====================================================~n", [Username, ClientId, emqttd_message:format(Message)]),
+  log("~nmessage log (message.acked):~nclient(~s/~s) acked: ~s~n=====================================================~n", [Username, ClientId, emqttd_message:format(Message)]),
   request_message_ask_hook(Topic, Payload, ClientId, Username, message_ask, env_http_request()),
   {ok, Message}.
 
@@ -124,6 +124,10 @@ request_message_hook(Topic, Payload, ClientId, Username, Action, #http_request{m
   ],
   request(Method, Url, Params).
 
+%%--------------------------------------------------------------------
+%% Request Ask
+%%--------------------------------------------------------------------
+
 request_message_ask_hook(Topic, Payload, ClientId, Username, Action, #http_request{method = Method, url = Url, server_key = ServerKey}) ->
   Mod = message,
   Params = [
@@ -136,4 +140,81 @@ request_message_ask_hook(Topic, Payload, ClientId, Username, Action, #http_reque
     , {topic, Topic}
     , {payload, Payload}
   ],
-  request(Method, Url, Params).
+  case requestSync(Method, Url, Params) of {ok, 200, Body} ->
+    Json = trimBOM(list_to_binary(Body)),
+    IsJson = jsx:is_json(Json),
+    if
+      IsJson ->
+        handleAskResult(ClientId, Username, Json);
+      true ->
+        {error, "Auth Failure"}
+    end;
+    {ok, Code, _Body} ->
+      {error, Code};
+    {error, Error} ->
+      {error, Error}
+  end.
+
+handleAskResult(ClientId, Username, Json) ->
+  JSONBody = jsx:decode(Json),
+  case lists:keyfind(<<"sub_list">>, 1, JSONBody) of {_, SubList} ->
+    handleAskSub(ClientId, Username, SubList);
+    _ ->
+      true
+  end,
+  case lists:keyfind(<<"pub_list">>, 1, JSONBody) of {_, PubList} ->
+    handleAskPub(ClientId, Username, PubList);
+    _ ->
+      true
+  end,
+  ok.
+
+handleAskPub(ClientId, _Username, PubList) when is_list(PubList) ->
+  try
+    lists:map(fun({Topic, Payload}) ->
+      Msg = emqttd_message:make(ClientId, 1, Topic, Payload),
+      emqttd:publish(Msg#mqtt_message{retain = true}),
+      {Topic, Payload} end, PubList)
+  catch
+    throw:Term ->
+      Term;
+    exit:Reason ->
+      Reason;
+    error:Reason ->
+      Reason
+  end,
+  ok;
+handleAskPub(_, _, _) ->
+  ok.
+
+handleAskSub(ClientId, Username, SubList) when is_list(SubList) ->
+  try
+    Client = emqttd_cm:lookup(ClientId),
+    ClientPid = Client#mqtt_client.client_pid,
+    TopicTable = [{handleTopic(S, ClientId, Username), 1} || S <- SubList],
+    ClientPid ! {subscribe, TopicTable}
+  catch
+    throw:Term ->
+      Term;
+    exit:Reason ->
+      Reason;
+    error:Reason ->
+      Reason
+  end,
+  ok;
+handleAskSub(_, _, _) ->
+  ok.
+
+handleTopic(Topic, ClientId, Username) ->
+  try
+    FixUsername = replace_str(binary_to_list(Topic), ":username", binary_to_list(Username)),
+    FixAppId = replace_str(FixUsername, ":app_id", parser_app_by_clientId(ClientId)),
+    list_to_binary(FixAppId)
+  catch
+    throw:_Term ->
+      Topic;
+    exit:_Reason ->
+      Topic;
+    error:_Reason ->
+      Topic
+  end.
