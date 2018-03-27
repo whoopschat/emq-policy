fe726fa76611f9236637f4bd7e888e16dcac4961
liangmayong <liangmayong@surevideo.me>
2018-03-27 19:19:34 +0800

    add acl hook

--- src/emq_policy_server_module_acl.erl
+++ src/emq_policy_server_module_acl.erl
@@ -30,7 +30,10 @@
 -include("emq_policy_server.hrl").
 -include_lib("emqttd/include/emqttd.hrl").
 
--import(emq_policy_server_util_format, [parser_app_by_clientId/1, parser_device_by_clientId/1, validate_clientId/2]).
+-import(emq_policy_server_util_format, [parser_app_by_clientId/1, parser_device_by_clientId/1, validate_clientId/2, validate_boolean/1]).
+-import(emq_policy_server_util_http, [requestSync/3, env_http_request/0]).
+-import(emq_policy_server_util_binary, [trimBOM/1]).
+-import(emq_policy_server_util_logger, [errorLog/2, infoLog/2]).
 
 %% Callbacks
 -export([init/1, check_acl/2, reload_acl/1, description/0]).
@@ -50,8 +53,52 @@ access(publish, ClientId, Topic) ->
   IsTopic = string:str(binary_to_list(Topic), "$" ++ App ++ "/") > 0,
   if
     IsTopic ->
-      allow;
+      request_acl_hook(ClientId, Topic, publish_acl, env_http_request());
     true ->
+      deny
+  end.
+
+
+%%--------------------------------------------------------------------
+%% Request Hook
+%%--------------------------------------------------------------------
+
+request_acl_hook(ClientId, Topic, Action, #http_request{method = Method, url = Url, server_key = ServerKey}) ->
+  Mod = acl,
+  Params = [
+    {server_key, ServerKey}
+    , {app_id, parser_app_by_clientId(ClientId)}
+    , {module, Mod}
+    , {action, Action}
+    , {client_id, ClientId}
+    , {topic, Topic}
+  ],
+  case requestSync(Method, Url, Params) of {ok, Code, Body} ->
+    infoLog("~nrequest_acl_hook ~nCode: ~p,~nBody: ~p~n", [Code, Body]),
+    Json = trimBOM(list_to_binary(Body)),
+    IsJson = jsx:is_json(Json),
+    if
+      IsJson ->
+        handle_request_result(Json);
+      true ->
+        errorLog("~Acl Request JSON Format Error : ~p~n", [Body]),
+        deny
+    end;
+    {error, Error} ->
+      errorLog("~naction: ~p~nError: ~p~n", [Action, Error]),
+      deny
+  end.
+
+handle_request_result(Json) ->
+  JSONBody = jsx:decode(Json),
+  case lists:keyfind(<<"is_allow">>, 1, JSONBody) of {_, IsAllow} ->
+    IsAllowFlag = validate_boolean(IsAllow),
+    if IsAllowFlag ->
+      allow;
+      true ->
+        deny
+    end;
+    _ ->
       deny
   end.
 
