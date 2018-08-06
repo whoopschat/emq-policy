bac30b68113fa2168a12b5314b8d0d0ad0b47f5e
liangmayong <liangmayong@surevideo.me>
2018-08-06 20:26:33 +0800

    fix bugs

--- src/emq_policy_app.erl
+++ src/emq_policy_app.erl
@@ -9,7 +9,7 @@ start(_StartType, _StartArgs) ->
   {ok, Sup} = emq_policy_module_super:start_link(),
   ok = emqttd_access_control:register_mod(auth, emq_policy_module_auth, []),
   ok = emqttd_access_control:register_mod(acl, emq_policy_module_acl, []),
-  emq_policy_server_module_hook:load(application:get_all_env()),
+  emq_policy_module_hook:load(application:get_all_env()),
   {ok, Sup}.
 
 stop(_State) ->
